d6a3ec2727f185c1cef9f778ebc6c285bc5fb925
TheChukwuma <egwuonwuchukwuma74@gmail.com>
2022-09-21 21:04:16 +0100

    implement the notification service save method

--- src/main/java/com/decagon/rewardyourteacherapi11bjavapodf2/service/NotificationService.java
+++ src/main/java/com/decagon/rewardyourteacherapi11bjavapodf2/service/NotificationService.java
@@ -1,18 +1,13 @@
 package com.decagon.rewardyourteacherapi11bjavapodf2.service;
 
+import com.decagon.rewardyourteacherapi11bjavapodf2.enums.NotificationType;
 import com.decagon.rewardyourteacherapi11bjavapodf2.model.Notification;
+import com.decagon.rewardyourteacherapi11bjavapodf2.model.User;
 import com.decagon.rewardyourteacherapi11bjavapodf2.response.ApiResponse;
 
 import java.math.BigDecimal;
 
 public interface NotificationService {
 
-
-     Notification fundStudentWalletNotification(Long id, BigDecimal amount);
-     ApiResponse<Notification> appreciationNotification(Long senderId, Long receiverId);
-     Notification successfulWithdrawNotification(Long userId, BigDecimal amount);
-     Notification receiveFundsNotification(Long receiverId, BigDecimal amount);
-     Notification sendFundsNotification(Long senderId, BigDecimal amount);
-     void TransferNotification(Long senderTransactionId, Long receiverTransactionId, BigDecimal amount);
-
+     Notification saveNotification(User user, String message, NotificationType notificationType);
 }
